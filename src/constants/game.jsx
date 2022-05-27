f25298debe5f3873b558cee503eb0363d32ffebc
Julian <julicogui@gmail.com>
2022-05-26 22:03:19 -0300

    constants folder created & sass setup

---
+++ src/constants/game.jsx
@@ -0,0 +1,24 @@
+const AVAILABLE_SHIPS = [
+  {
+    name: 'carrier',
+    length: 5
+  },
+  {
+    name: 'cruiser',
+    length: 3
+  },
+  {
+    name: 'cruiser II',
+    length: 3
+  },
+  {
+    name: 'cruiser III',
+    length: 3
+  },
+  {
+    name: 'submarine',
+    length: 2
+  }
+];
+
+export default AVAILABLE_SHIPS;
