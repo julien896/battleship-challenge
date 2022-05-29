f9fc884926c484981292787d05964ac644838ec9
Julian <julicogui@gmail.com>
2022-05-29 02:33:46 -0300

    feature/boards & style refactoring

--- src/components/CpuBoard/CpuBoard.jsx
+++ src/components/CpuBoard/CpuBoard.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { generateEmptyLayout, stateToClassName } from '../../constants/layout';
+import '../../styles/base/board.scss';
 
 function CpuBoard() {
-  return <div>CPU board</div>;
+  const squares = generateEmptyLayout().map((square, index) => (
+    <div
+      className={`square ${stateToClassName[square]}`}
+      id={`square-${index}`}
+    />
+  ));
+  return (
+    <div className="board-container">
+      <h2 className="player-name">CPU</h2>
+      <div className="board">{squares}</div>
+    </div>
+  );
 }
 
 export default CpuBoard;
--- src/components/PlayerBoard/PlayerBoard.jsx
+++ src/components/PlayerBoard/PlayerBoard.jsx
@@ -1,20 +1,20 @@
-/* eslint-disable */
 import React from 'react';
 import { generateEmptyLayout, stateToClassName } from '../../constants/layout';
-import './playerBoard.scss';
-
+import '../../styles/base/board.scss';
 
 function PlayerBoard() {
-    const squares = generateEmptyLayout().map((square, index) => {
-        return (
-          <div
-            className={`square ${stateToClassName[square]}`}
-            key={`square-${index}`}
-            id={`square-${index}`}
-          />
-        );
-      });
-  return <div className='board'>{squares}</div>;
+  const squares = generateEmptyLayout().map((square, index) => (
+    <div
+      className={`square ${stateToClassName[square]}`}
+      id={`square-${index}`}
+    />
+  ));
+  return (
+    <div className="board-container">
+      <h2 className="player-name">Player1</h2>
+      <div className="board">{squares}</div>
+    </div>
+  );
 }
 
 export default PlayerBoard;
